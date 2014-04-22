67452be71b4fc10192dd75ac75e72f8662b15e1d
unknown <drewdudash@gmail.com>
2014-04-22 11:56:37 -0400

    add filter-key and foldl-key with basic tests in same file

--- key-foo.rkt
+++ key-foo.rkt
@@ -1,6 +1,43 @@
 #lang racket
 
+(require rackunit)
+
+; (foldl proc init lst)
+
+(define (foldl-key proc init lst key)
+  (let iter ([accum init]
+             [lst lst])
+    (cond [(null? lst) accum]
+          [else (iter (proc (key (first lst))
+                            accum)
+                      (rest lst))])))
+
+(let ([lst (map range (range 10))])
+  (test-equal? "foldl-key"
+               (foldl-key + 0 lst length)
+               (foldl (lambda (item accum)
+                        (+ accum (length item)))
+                      0
+                      lst)))
+
 ; (filter pred lst) remove if pred true
+
+(define (filter-key pred lst key)
+  (foldl-key (lambda (item accum)
+               (if (pred item)
+                   (cons item accum)
+                   accum))
+             '()
+             (reverse lst)
+             key))
+
+(let ([lst (map range (range 10))])
+  (test-equal? "filter-key"
+               (filter-key (curry > 5)
+                           lst
+                           length)
+               (filter (compose (curry > 5) length)
+                       lst)))
 
 ; (filter-not pred lst) remove if pred false
 
@@ -9,8 +46,8 @@
 ; (partition pred lst) same as (values (filter pred lst) (filter (negate pred) lst))
 
 ; (count proc lst ...+) same as (length (filter proc lst ...))
-
-; (foldl proc init lst)
+    
+             
 
 ; (foldr proc init lst)
 
